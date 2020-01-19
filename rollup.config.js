ec42b7f018d5767bf5068a9d1876ff823a7915e2
Matt Travi <programmer@travi.org>
2020-01-18 22:00:50 -0600

    build(rollup): added the node resolve plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,10 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
 import autoExternal from 'rollup-plugin-auto-external';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
-  plugins: [autoExternal()],
+  plugins: [autoExternal(), nodeResolve({mainFields: ['module']})],
   output: [
     {file: 'lib/index.cjs.js', format: 'cjs', sourcemap: true},
     {file: 'lib/index.es.js', format: 'es', sourcemap: true}
