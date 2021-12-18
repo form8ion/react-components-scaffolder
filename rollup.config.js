0641e4bd930c2eb3b5ba94fc466f44b50256fd0e
Matt Travi <programmer@travi.org>
2021-12-18 17:26:12 -0600

    build(rollup): updated the references to the node resolve plugin to use the scoped version

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 /* eslint import/no-extraneous-dependencies: ['error', {'devDependencies': true}] */
 import autoExternal from 'rollup-plugin-auto-external';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import nodeResolve from '@rollup/plugin-node-resolve';
 
 export default {
   input: 'src/index.js',
